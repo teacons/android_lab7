79df7ffbbb6e787dccb5f060dd922436cd62dddb
teacons <teacons@fbear.ru>
2020-12-08 20:30:43 +0300

    Started and bounded

--- app/src/main/java/ru/fbear/lab7/MainActivity.kt
+++ app/src/main/java/ru/fbear/lab7/MainActivity.kt
@@ -1,25 +1,109 @@
 package ru.fbear.lab7
 
+import android.annotation.SuppressLint
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
-import android.os.Bundle
+import android.content.ServiceConnection
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("HandlerLeak")
+    inner class IncomingHandler : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                DOWNLOAD_URL -> {
+                    result.text = msg.data.getString(URL)
+                }
+                else -> super.handleMessage(msg)
+            }
+        }
+    }
+    companion object {
+        private const val DOWNLOAD_URL = 1
+        private const val URL = "url"
+    }
+
+    private lateinit var mMessenger: Messenger
+
+    private var mService: Messenger? = null
+    private var bound: Boolean = false
+
+    private val mConnection = object : ServiceConnection {
+
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            mService = Messenger(service)
+            bound = true
+        }
+
+        override fun onServiceDisconnected(className: ComponentName) {
+            bound = false
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Intent(this, MyService::class.java).also { intent ->
+            bindService(intent, mConnection, Context.BIND_AUTO_CREATE)
+        }
+        mMessenger = Messenger(IncomingHandler())
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (bound) {
+            unbindService(mConnection)
+            bound = false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        download_btn.setOnClickListener {
+        download_bounded.setOnClickListener {
             val urlText =
                 if (url.text.isNotEmpty()) {
                     url.text.toString()
                 } else {
                     "https://old.fbear.ru/image/android_cat.webp"
                 }
-            val intent = Intent(this, MyService::class.java)
-            intent.putExtra("url", urlText)
-            startService(intent)
+            sendBounded(urlText)
+        }
+        download_started.setOnClickListener {
+            val urlText =
+                if (url.text.isNotEmpty()) {
+                    url.text.toString()
+                } else {
+                    "https://old.fbear.ru/image/android_cat.webp"
+                }
+            sendStarted(urlText)
         }
     }
+
+    private fun sendBounded(url: String) {
+        if (!bound) return
+        val bundle = Bundle().apply {
+            putString(URL, url)
+        }
+        val msg: Message = Message.obtain(null, DOWNLOAD_URL, 0, 0).apply {
+            data = bundle
+            replyTo = mMessenger
+        }
+        try {
+            mService?.send(msg)
+        } catch (e: RemoteException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun sendStarted(url: String) {
+        val intent = Intent(this, MyService::class.java)
+        intent.putExtra("url", url)
+        startService(intent)
+
+    }
+
 }
 
--- app/src/main/java/ru/fbear/lab7/MyService.kt
+++ app/src/main/java/ru/fbear/lab7/MyService.kt
@@ -1,53 +1,96 @@
 package ru.fbear.lab7
 
+import android.annotation.SuppressLint
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.os.IBinder
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import android.os.*
+import kotlinx.coroutines.*
 import java.io.FileOutputStream
-import java.io.IOException
 import java.net.URL
 
 
 class MyService : Service() {
 
+    private lateinit var mMessenger: Messenger
+
+    private var count = 0
+
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         runBlocking {
-            withContext(Dispatchers.IO) {
-                var outputStream: FileOutputStream? = null
-                try {
-                    val urlString = intent.getStringExtra("url")
-                    val url = URL(urlString)
-                    val inputStream = url.openStream()
-                    val img = BitmapFactory.decodeStream(inputStream)
-                    outputStream = openFileOutput("test.png", Context.MODE_PRIVATE)
-                    img.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-
-                    val i = Intent().apply {
-                        action = "ru.fbear.lab7.DOWNLOAD_COMPLETE"
-                        putExtra("ru.fbear.lab7.broadcast.Message", "$filesDir/test.png")
-                        addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
-                    }
-                    sendBroadcast(i)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                } finally {
-                    try {
-                        outputStream?.close()
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
-                }
-            }
+            val path = downloadImage(intent.getStringExtra(URL), "$count.png")
+            count++
+            path?.let { broadcast(it) }
         }
-        stopSelf(startId)
         return START_STICKY
     }
 
-    override fun onBind(intent: Intent?): IBinder? = null
+    override fun onBind(intent: Intent?): IBinder? {
+        mMessenger = Messenger(IncomingHandler())
+        return mMessenger.binder
+    }
+
+    companion object {
+        private const val DOWNLOAD_URL = 1
+        private const val URL = "url"
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    private suspend fun downloadImage(urlString: String?, filename: String): String? {
+        return withContext(Dispatchers.IO) {
+            var outputStream: FileOutputStream? = null
+            try {
+                val url = URL(urlString)
+                val inputStream = url.openStream()
+                val img = BitmapFactory.decodeStream(inputStream)
+                outputStream = openFileOutput(filename, MODE_PRIVATE)
+                img.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                "$filesDir/$filename"
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            } finally {
+                outputStream?.close()
+            }
+        }
+    }
+
+    private fun broadcast(path: String) {
+        val i = Intent().apply {
+            action = "ru.fbear.lab7.DOWNLOAD_COMPLETE"
+            putExtra("ru.fbear.lab7.broadcast.Message", path)
+            addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
+        }
+        sendBroadcast(i)
+    }
+
+    @SuppressLint("HandlerLeak")
+    inner class IncomingHandler : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                DOWNLOAD_URL -> {
+                    runBlocking {
+                        val path = downloadImage(msg.data.getString(URL), "$count.png")
+                        count++
+                        path?.let {
+                            val bundle = Bundle().apply {
+                                putString(URL, it)
+                            }
+                            val mes: Message = Message.obtain(null, DOWNLOAD_URL).apply {
+                                data = bundle
+                            }
+                            try {
+                                val repMessenger = msg.replyTo
+                                repMessenger.send(mes)
+                            } catch (e: RemoteException) {
+                                e.printStackTrace()
+                            }
+                        }
+                    }
+                }
+                else -> super.handleMessage(msg)
+            }
+        }
+    }
 }
