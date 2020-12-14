056d7035e6977c97aea1fda9641a075886e2b949
teacons <teacons@fbear.ru>
2020-12-14 13:10:03 +0300

    Edit AndroidManifest.xml in module receiver (removed tag receiver); Added scope to launch coroutine in MyService.kt;  Changed logic of saving image to disk.

--- app/src/main/java/ru/fbear/lab7/MyService.kt
+++ app/src/main/java/ru/fbear/lab7/MyService.kt
@@ -3,11 +3,11 @@ package ru.fbear.lab7
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.IBinder
+import android.webkit.URLUtil
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.FileOutputStream
 import java.io.IOException
@@ -17,36 +17,41 @@ import java.net.URL
 class MyService : Service() {
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        runBlocking {
-            withContext(Dispatchers.IO) {
-                var outputStream: FileOutputStream? = null
-                try {
-                    val urlString = intent.getStringExtra("url")
-                    val url = URL(urlString)
-                    val inputStream = url.openStream()
-                    val img = BitmapFactory.decodeStream(inputStream)
-                    outputStream = openFileOutput("test.png", Context.MODE_PRIVATE)
-                    img.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        GlobalScope.launch {
+            intent.getStringExtra("url")?.let { downloadImage(it) }
+        }
+        stopSelf(startId)
+        return START_NOT_STICKY
+    }
 
-                    val i = Intent().apply {
-                        action = "ru.fbear.lab7.DOWNLOAD_COMPLETE"
-                        putExtra("ru.fbear.lab7.broadcast.Message", "$filesDir/test.png")
-                        addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
-                    }
-                    sendBroadcast(i)
-                } catch (e: Exception) {
+    @Suppress("BlockingMethodInNonBlockingContext")
+    private suspend fun downloadImage(urlString: String) {
+        withContext(Dispatchers.IO) {
+            var outputStream: FileOutputStream? = null
+            try {
+                val url = URL(urlString)
+                val connect = url.openConnection()
+                val mimeType = connect.contentType
+                val fileName = URLUtil.guessFileName(connect.url.toString(), null, mimeType)
+                val inputStream = connect.getInputStream()
+                outputStream = openFileOutput(fileName, Context.MODE_PRIVATE)
+                outputStream.write(inputStream.readBytes())
+
+                val i = Intent().apply {
+                    action = "ru.fbear.lab7.DOWNLOAD_COMPLETE"
+                    putExtra("ru.fbear.lab7.broadcast.Message", "$filesDir/$fileName")
+                }
+                sendBroadcast(i)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                try {
+                    outputStream?.close()
+                } catch (e: IOException) {
                     e.printStackTrace()
-                } finally {
-                    try {
-                        outputStream?.close()
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
                 }
             }
         }
-        stopSelf(startId)
-        return START_STICKY
     }
 
     override fun onBind(intent: Intent?): IBinder? = null
